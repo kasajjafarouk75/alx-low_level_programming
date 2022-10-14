409c5769289f751227a95d0e2433943b281375b6
kasajjafarouk75 <kasajjafarouk>
2022-10-13 19:58:24 -0700

    fghl

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- *  main - Entry
- 
- *  Return: Always 0
- 
- */
+ *  *main- Serves as the entry point for the program
+ *   *
+ *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
