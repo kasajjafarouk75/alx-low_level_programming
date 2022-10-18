31fb92c51c5c30cb6e33d38e7d10faf6b1f0662f
kasajjafarouk75 <kasajjafarouk>
2022-10-17 18:41:33 -0700

    i hate betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* finding if a number is less than zero then print negative;
+/*
+ * finding if a number is less than zero then print negative;
  * if the number is greater than zero then print positive;
- * if the number is equal to zero then print zero; */
-int main()
-{int n;
+ * if the number is equal to zero then print zero;
+ */
+int main(void)
+{
+	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0) {printf("is negative");}
-	else if (n > 0) {printf("is positive");}
-	else (n == 0) {printf("is zero");}
-	return 0;}
+
+if
+	(n < 0) {printf("is negative"); }
+
+else if
+(n > 0) {printf("is positive"); }
+
+else
+	(n == 0) {printf("is zero"); }
+
+(return 0);
+}
