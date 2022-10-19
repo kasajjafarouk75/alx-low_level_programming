1238f59eb5367278f17ea3293e8765b9398b046e
kasajjafarouk75 <kasajjafarouk>
2022-10-19 06:40:02 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  main - print _putchar
+ *  is written by farouk
+ *  Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
